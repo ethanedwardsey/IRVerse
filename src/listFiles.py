eb0a974bc5a012b7f8875e657c9dbfb2c000de20
ethanedwards <ethanmedwards@gmail.com>
2022-05-20 11:53:51 -0400

    first commit
    Initial Commit

---
+++ src/listFiles.py
@@ -0,0 +1,4 @@
+import os
+modelList = os.listdir('../static/textures/SpeakEasy_BAKED_textures/SpeakEasy_BAKED_textures_transparent')
+#pre_res = ['models/' + sub for sub in modelList]
+print(modelList)
