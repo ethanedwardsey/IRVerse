4c0981ef1f24fe8b837a5d58bf054a101657d17a
ethanedwards <ethanmedwards@gmail.com>
2022-05-20 13:57:09 -0400

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import './style.css'
+import 'style.css'
 import * as dat from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
